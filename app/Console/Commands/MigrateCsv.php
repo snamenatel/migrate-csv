02a13a7aa2a474e6ecc6a67e3fe0817c79de846b
snamenatel <snamenatel@gmail.com>
2022-03-24 22:47:00 +0700

    Added customer model

---
+++ app/Console/Commands/MigrateCsv.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+class MigrateCsv extends Command
+{
+    const ROW_DIVIDER = '/\R/';
+    const COL_DIVIDER = ',';
+    protected            $signature   = 'migrate:csv {--file=random.csv}';
+    protected            $description = 'Миграция данных из csv файлов в таблицу customers';
+    protected array      $originalRows;
+    protected Collection $headRows;
+    protected array      $contentRows;
+    protected int        $countCols   = 0;
+    protected array      $errors;
+
+
+    public function handle()
+    {
+        $this->info('Начало выполнения команды:' . $this->description);
+        $this->parseFile();
+        $this->createCustomers();
+    }
+
+    public function createCustomers(): void
+    {
+        foreach($this->contentRows as $item) {
+            $this->createCustomers($item);
+        }
+    }
+
+    public function createCustomerItem(Collection $item): void
+    {
+        Validator::make($item->toArray(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email:rfc,dns',
+            'age' => 'required|integer|min:18|max:99',
+            'location' => 'string',
+        ]);
+    }
+
+    public function parseFile(): void
+    {
+        $this->originalRows = $this->getRows($this->getFileContent());
+        if (count($this->originalRows) < 2) {
+            $this->error('Пустой файл');
+            exit();
+        }
+
+        $this->headRows    = collect(explode(self::COL_DIVIDER, $this->originalRows[0]))->map(fn($i) => trim($i));
+        $this->countCols   = $this->headRows->count();
+        $this->contentRows = $this->getRowsColumns(array_slice($this->originalRows, 1));
+    }
+
+    public function getFileContent(): string
+    {
+        return Storage::disk('local')->get('resource' . DIRECTORY_SEPARATOR . $this->option('file'));
+    }
+
+    public function getRows(string $content): array
+    {
+        return array_map(fn($s) => trim($s, '"'), preg_split(self::ROW_DIVIDER, $content));
+    }
+
+    public function getRowsColumns(array $rows): array
+    {
+        $result = [];
+        foreach ($rows as $item) {
+            $result[] = $this->headRows->combine(
+                array_pad($this->clearStrings(explode(self::COL_DIVIDER, $item)), $this->countCols, '')
+            );
+        }
+
+        return $result;
+    }
+
+    public function clearStrings(array $cols): array
+    {
+        return array_map(function ($item) {
+            return trim(trim($item, '"'));
+        }, $cols);
+    }
+}
